a5614218535899bc301380515eaa0049c57df3f0
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2021-10-10 14:32:32 +0900

    build: index.html 캐싱 설정 제거

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store';
 import mitt from 'mitt';
+import './registerServiceWorker'
 
 let emitter = mitt();
 let app = createApp(App)
